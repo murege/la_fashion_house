fb96c1403d8a6b676fb640e2d5408638f1b5dc06
murege <muregelucy@gmail.com>
2026-06-11 10:57:43 +0300

    fixed include paths and login page errors

---
+++ week 3/navbar.php
@@ -0,0 +1,73 @@
+<nav class="navbar navbar-expand-lg">
+
+<div class="container">
+
+<a class="navbar-brand"
+href="index.php">
+LA Fashion House
+</a>
+
+<button
+class="navbar-toggler bg-light"
+data-bs-toggle="collapse"
+data-bs-target="#menu">
+
+<span class="navbar-toggler-icon"></span>
+
+</button>
+
+<div class="collapse navbar-collapse"
+id="menu">
+
+<ul class="navbar-nav ms-auto">
+
+<li class="nav-item">
+<a class="nav-link"
+href="index.php">
+Home
+</a>
+<li class="nav-item">
+    <a class="nav-link" href="admin_dashboard.php">
+        Admin Dashboard
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="user_dashboard.php">
+        Dashboard
+    </a>
+</li>
+<li class="nav-item">
+<a class="nav-link"
+href="products.php">
+Products
+</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link"
+href="cart.php">
+Cart
+</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link"
+href="login.php">
+Login
+</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link"
+href="register.php">
+Register
+</a>
+</li>
+
+</ul>
+
+</div>
+
+</div>
+
+</nav>
